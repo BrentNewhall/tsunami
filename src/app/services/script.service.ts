9cc67f20b229856b824192506c8f3bd210397ac1
Brent P. Newhall <brent@brentnewhall.com>
2020-03-06 23:33:16 -0500

    Refactor script parsing code

--- src/app/services/script.service.ts
+++ src/app/services/script.service.ts
@@ -14,7 +14,45 @@ export class ScriptService {
     this.http.get('assets/script.txt', {responseType: 'text'})
       .subscribe(data => this.readScript(data));
   }
-  readScript(data): void {
+
+  private parseCharacter( line: String ): Object {
+    let fields = line.split("\t");
+    let character = {};
+    if( fields.length > 1 ) {
+      character["charName"] = fields[1];
+    }
+    if( fields.length > 2 ) {
+      const extraFields = this.parseExtraFields( fields, 2 );
+      character = {...character, ...extraFields};
+    }
+    return character;
+  }
+
+  private parseLine( line: String ): Object {
+    let fields = line.split("\t");
+    let scriptLine = {};
+    scriptLine["charName"] = fields[0];
+    scriptLine["line"] = fields[1];
+    if( fields.length > 2 ) {
+      const extraFields = this.parseExtraFields( fields, 2 );
+      scriptLine = {...scriptLine, ...extraFields};
+    }
+    return scriptLine;
+  }
+
+  private parseExtraFields( fields, start ): Object {
+    let result = {};
+    fields.splice(0,start);
+    for( const field of fields ) {
+      if( field.includes(":") ) {
+        const values = field.split(":");
+        result[values[0].trim().toLowerCase()] = values[1].trim();
+      }
+    }
+    return result;
+  }
+
+  private readScript(data): void {
     let section: String = "header";
     for( const line of data.split("\n") ) {
       const lowerLine = line.toLowerCase().trim();
@@ -23,31 +61,12 @@ export class ScriptService {
       }
       else if( section === "header" ) {
         if( lowerLine.substring(0,9) === "character" ) {
-          let fields = line.split("\t");
-          let character = {};
-          if( fields.length > 1 ) {
-            character["charName"] = fields[1];
-          }
-          if( fields.length > 2 ) {
-            fields.splice(0,2);
-            for( const field of fields ) {
-              const values = field.split(":");
-              character[values[0].trim().toLowerCase()] = values[1].trim();
-            }
-          }
-          this.characters.push( character );
+          this.characters.push( this.parseCharacter( line ) );
         }
       }
       else if( section === "body" ) {
-        let fields = line.split("\t");
-        let scriptLine = {};
-        scriptLine["charName"] = fields[0];
-        scriptLine["line"] = fields[1];
-        this.scriptLines.push( scriptLine );
+        this.scriptLines.push( this.parseLine( line ) );
       }
-      console.log( section, `"${lowerLine}"` );
     }
-    console.log( "Characters", this.characters );
-    console.log( "Lines:", this.scriptLines );
   }
 }
